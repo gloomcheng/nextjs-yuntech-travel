5434eb0c6e39458abc04e04e158c99ce290762f1
Fuyuan Cheng <gloomcheng@gmail.com>
2024-06-14 17:02:06 +0800

    feat: Add api/items

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,9 +1,12 @@
 /**
  * @author Fuyuan Cheng <gloomcheng@gmail.com>
  */
+import Image from "next/image";
+
 export default function Card({item}) {
     return (
         <div className="border">
+            <Image src={item.Picture.PictureUrl1} alt={item.Picutre.PictureDescription1} />
             <div>{item.ScenicSpotName}</div>
         </div>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@
 
 import { useState, useEffect } from 'react';
 import Link from "next/link";
+import Image from "next/image";
 import { 
   Navbar, 
   NavbarBrand, 
@@ -25,29 +26,15 @@ import Card from './components/Card';
 export default function Home() {
 
   const [items, setItems] = useState([]);
-  const tokenUrl = 'https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token';
-  const apiUrl = 'https://tdx.transportdata.tw/api/basic/v2/Tourism/ScenicSpot/YunlinCounty';
 
   useEffect(() => {
-    const getToken = async () => {
-      const clientId = process.env.TDX_CLIENT_ID;
-      const clientSecret = process.env.TDX_CLIENT_SECRET;
-
-      const tokenParams = new URLSearchParams();
-      tokenParams.append('grant_type', 'client_credentials');
-      tokenParams.append('client_id', clientId);
-      tokenParams.append('client_secret', clientSecret);
-
-      const tokenResponse = await fetch(tokenUrl, {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded'
-        },
-        body: tokenParams.toString()
-      });
-    };
-
-
+    async function fetchData() {
+      const response = await fetch('api/items');
+      const data = await response.json();
+      console.log(data);
+      setItems(data);
+    }
+    fetchData();
   }, []);
 
 
@@ -57,7 +44,7 @@ export default function Home() {
       <div className="container mx-auto">
       <Navbar fluid className="bg-cyan-800">
         <NavbarBrand as={Link} href="/">
-          <img src="https://www.yuntech.edu.tw/images/website_png/Group_640.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+          <Image src="https://www.yuntech.edu.tw/images/website_png/Group_640.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
           <span className="self-center whitespace-nowrap text-xl font-semibold text-white">YunTech Travale</span>
         </NavbarBrand>
         <NavbarToggle />
@@ -77,18 +64,18 @@ export default function Home() {
 
       <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
         <Carousel>
-          <img src="/banner/banner-1.jpg" alt="由 WU PEI HSUAN - 自己的作品, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=110297869" />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-          <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+          <Image src="/banner/banner-1.jpg" alt="由 WU PEI HSUAN - 自己的作品, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=110297869" />
+          <Image src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+          <Image src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+          <Image src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+          <Image src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
         </Carousel>
       </div>
 
-      <div class="container mx-auto">
-        <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-          { items.map( item =>
-            <Card item={item}/>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+          { items.map((item, index) =>
+            <Card item={item} key={index} />
           )}
         </div>
       </div>
