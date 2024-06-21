470f2e4a725d90c2196c2b51972b61dd8723ed43
Fuyuan Cheng <gloomcheng@zhelang.com.tw>
2024-06-21 12:36:15 +0800

    feat: add AutoSizeImage component

--- src/app/page.js
+++ src/app/page.js
@@ -2,34 +2,33 @@
  * @author Fuyuan Cheng <gloomcheng@gmail.com>
  */
 
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import Link from "next/link";
-import Image from "next/image";
-import { 
-  Navbar, 
-  NavbarBrand, 
-  NavbarCollapse, 
-  NavbarLink, 
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
   NavbarToggle,
   Footer,
-  FooterCopyright, 
-  FooterLink, 
+  FooterCopyright,
+  FooterLink,
   FooterLinkGroup,
   Carousel,
   Button,
-  DarkThemeToggle
+  DarkThemeToggle,
 } from "flowbite-react";
-import Card from './components/Card';
+import Card from "../components/Card";
+import AutoSizeImage from "../components/AutoSizeImage";
 
 export default function Home() {
-
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('api/items');
+      const response = await fetch("api/items");
       const data = await response.json();
       console.log(data);
       setItems(data);
@@ -37,58 +36,81 @@ export default function Home() {
     fetchData();
   }, []);
 
-
   return (
     <>
-    <div className="bg-cyan-800">
-      <div className="container mx-auto">
-      <Navbar fluid className="bg-cyan-800">
-        <NavbarBrand as={Link} href="/">
-          <Image src="https://www.yuntech.edu.tw/images/website_png/Group_640.png" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-          <span className="self-center whitespace-nowrap text-xl font-semibold text-white">YunTech Travale</span>
-        </NavbarBrand>
-        <NavbarToggle />
-        <NavbarCollapse>
-          <NavbarLink href="#" className="text-white">
-            <span className="px-4 py-2 hover:text-yellow-500 hover:border-yellow-500 hover:border-b-2">交通</span>
-          </NavbarLink>
-          <NavbarLink as={Link} href="#" className="text-white">
-            景點
-          </NavbarLink>
-          <NavbarLink href="#" className="text-white">關於我們</NavbarLink>
-        </NavbarCollapse>
-        <DarkThemeToggle />
-      </Navbar>
+      <div className="bg-cyan-800">
+        <div className="container mx-auto">
+          <Navbar fluid className="bg-cyan-800">
+            <NavbarBrand as={Link} href="/">
+              <AutoSizeImage
+                src="https://www.yuntech.edu.tw/images/website_png/Group_640.png"
+                className="mr-3 h-6 sm:h-9"
+                alt="Flowbite React Logo"
+              />
+              <span className="self-center whitespace-nowrap text-xl font-semibold text-white">
+                YunTech Travale
+              </span>
+            </NavbarBrand>
+            <NavbarToggle />
+            <NavbarCollapse>
+              <NavbarLink href="#" className="text-white">
+                <span className="px-4 py-2 hover:text-yellow-500 hover:border-yellow-500 hover:border-b-2">
+                  交通
+                </span>
+              </NavbarLink>
+              <NavbarLink as={Link} href="#" className="text-white">
+                景點
+              </NavbarLink>
+              <NavbarLink href="#" className="text-white">
+                關於我們
+              </NavbarLink>
+            </NavbarCollapse>
+            <DarkThemeToggle />
+          </Navbar>
+        </div>
       </div>
-    </div>
 
       <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
         <Carousel>
-          <Image src="/banner/banner-1.jpg" alt="由 WU PEI HSUAN - 自己的作品, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=110297869" />
-          <Image src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-          <Image src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-          <Image src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-          <Image src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+          <AutoSizeImage
+            src="/banner/banner-1.jpg"
+            alt="由 WU PEI HSUAN - 自己的作品, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=110297869"
+          />
+          <AutoSizeImage
+            src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+            alt="..."
+          />
+          <AutoSizeImage
+            src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+            alt="..."
+          />
+          <AutoSizeImage
+            src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+            alt="..."
+          />
+          <AutoSizeImage
+            src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+            alt="..."
+          />
         </Carousel>
       </div>
 
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-          { items.map((item, index) =>
+          {items.map((item, index) => (
             <Card item={item} key={index} />
-          )}
+          ))}
         </div>
       </div>
-      
 
       <Footer container>
-      <FooterCopyright href="#" by="Flowbite™" year={2022} />
-      <FooterLinkGroup>
-        <FooterLink href="#">About</FooterLink>
-        <FooterLink href="#">Privacy Policy</FooterLink>
-        <FooterLink href="#">Licensing</FooterLink>
-        <FooterLink href="#">Contact</FooterLink>
-      </FooterLinkGroup>
+        <FooterCopyright href="#" by="Flowbite™" year={2022} />
+        <FooterLinkGroup>
+          <FooterLink href="#">About</FooterLink>
+          <FooterLink href="#">Privacy Policy</FooterLink>
+          <FooterLink href="#">Licensing</FooterLink>
+          <FooterLink href="#">Contact</FooterLink>
+        </FooterLinkGroup>
       </Footer>
     </>
   );
